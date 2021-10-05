0e121a0f5e34a2fde006be2821dbf32251ad75c8
GarethLW <33941421+GarethLW@users.noreply.github.com>
2021-10-04 19:24:40 -0500

    Fix MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,18 +3,26 @@ import React from 'react';
 const MovieList = (props) => {
     const FavouriteComponent = props.favouriteComponent;
 
-    return (
-        <div className="d-flex">
-            {props.movies.map((movie, index) => (
-                <div key={movie.imdbID} className="image-container m-4">
-                    <img src={movie.Poster} alt='movie'></img>
-                    <div onClick={() => props.handleFavouritesClick(movie)} className="overlay addFav align-items-center">
-                        <FavouriteComponent/>
+    if (props.movies) {
+        return (
+            <div className="d-flex">
+                {props.movies.map((movie, index) => (
+                    <div key={movie.imdbID} className="image-container m-4">
+                        <img src={movie.Poster} alt='movie'></img>
+                        <div onClick={() => props.handleFavouritesClick(movie)} className="overlay addFav align-items-center">
+                            <FavouriteComponent />
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
-    )
+                ))}
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="d-flex">
+            </div>
+        )
+    }
 }
 
 export default MovieList;
